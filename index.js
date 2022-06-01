97df61763ccd812a6a824c0a8b15626c74487f30
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-06-01 14:51:18 +0300

    Add examples with different types function

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-const userInputString = prompt("Enter number");
+//! Example 1
+// const userInputString = prompt("Enter number");
 
-if (
-  userInputString === null ||
-  userInputString === "" ||
-  isNaN(Number(userInputString))
-) {
-  console.log("Error");
-} else {
-  console.log("Thanks for", userInputString);
+// if (
+//   userInputString === null ||
+//   userInputString === "" ||
+//   isNaN(Number(userInputString))
+// ) {
+//   console.log("Error");
+// } else {
+//   console.log("Thanks for", userInputString);
+// }
+
+//! Example 2
+//? Declaration
+function logStrMyFirstFunction() {
+  console.log("myFirstFunction");
 }
+
+logStrMyFirstFunction();
+
+//? Expression
+//* нельзя вызвать функции до её создание
+const logStrMySecondFunction = function() {
+  console.log("meSecondFunction");
+}
+
+logStrMySecondFunction();

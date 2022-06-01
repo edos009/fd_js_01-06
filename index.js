75bc193e37e8a62edcb4891ed315706c82a6f741
Eduard Tiutiunnyk <dostyutyunnik@gmail.com>
2022-06-01 15:41:34 +0300

    Example with function two numbers

--- index.js
+++ index.js
@@ -13,16 +13,66 @@
 
 //! Example 2
 //? Declaration
-function logStrMyFirstFunction() {
-  console.log("myFirstFunction");
-}
+// function logStrMyFirstFunction() {
+//   console.log("myFirstFunction");
+// }
 
-logStrMyFirstFunction();
+// logStrMyFirstFunction();
 
 //? Expression
 //* нельзя вызвать функции до её создание
-const logStrMySecondFunction = function() {
-  console.log("meSecondFunction");
-}
+// const logStrMySecondFunction = function() {
+//   console.log("meSecondFunction");
+// }
 
-logStrMySecondFunction();
+// logStrMySecondFunction();
+
+//! Example 3
+// const getSumTwoNumbers = function(){
+//   const inputFirstNumber = prompt('Введите любое число');
+//   const inputSecondNumber = prompt('Введите любое число');
+
+//   const checkFirstNumber =
+//     inputFirstNumber === null ||
+//     inputFirstNumber === "" ||
+//     isNaN(Number(inputFirstNumber));
+
+//   const checkSecondNumber =
+//     inputSecondNumber === null ||
+//     inputSecondNumber === "" ||
+//     isNaN(Number(inputSecondNumber));
+
+//     if (checkFirstNumber || checkSecondNumber) {
+//       console.log("Одно из чисел является не правильным, проверте!");
+//     } else {
+//       console.log(Number(inputFirstNumber) + Number(inputSecondNumber));
+//     }
+// }
+
+// getSumTwoNumbers();
+
+//*-------------------------------------------------------------------------
+
+const isInputNotNumber = function (value) {
+  return value === null || value === "" || isNaN(Number(value));
+};
+
+const getSumTwoNumbers = function (number1, number2) {
+  return Number(number1) + Number(number2);
+};
+
+const logResultINput = function () {
+  const inputFirstNumber = prompt("Введите любое число");
+  const inputSecondNumber = prompt("Введите любое число");
+
+  if (
+    isInputNotNumber(inputFirstNumber) ||
+    isInputNotNumber(inputSecondNumber)
+  ) {
+    console.log("Одно из чисел является не числом, проверте!");
+  } else {
+    console.log(getSumTwoNumbers(inputFirstNumber, inputSecondNumber));
+  }
+};
+
+logResultINput();
